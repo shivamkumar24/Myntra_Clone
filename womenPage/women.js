5bb720626a3dfbee7a6cc9888a04b73a25895c4a
Shivam Kumar <shivam2432000@gmail.com>
2022-09-29 01:43:12 +0530

    This is Day-3 Work

---
+++ womenPage/women.js
@@ -0,0 +1,34 @@
+
+document.querySelector("#banner").addEventListener("click",()=>{
+    window.location.href="womenProduct.html";
+})
+
+document.querySelector("#omg").addEventListener("click",()=>{
+    window.location.href="womenProduct.html";
+})
+
+document.querySelector("#badget").addEventListener("click",()=>{
+    window.location.href="womenProduct.html";
+})
+
+document.querySelector("#top_categories").addEventListener("click",()=>{
+    window.location.href="womenProduct.html";
+})
+
+document.querySelector("#top_brand").addEventListener("click",()=>{
+    window.location.href="womenProduct.html";
+})
+
+document.querySelector("#shop_categories").addEventListener("click",()=>{
+    window.location.href="womenProduct.html";
+})
+
+
+
+
+
+
+
+
+
+
---
+++ womenPage/womenProduct.js
@@ -0,0 +1,480 @@
+
+let women = [
+    {
+        image: "https://www.yoox.com/images/items/17/17267085pk_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DOLCE & GABBANA",
+        category: "Denim pantsAnkle Boots",
+        price: "947.00",
+        save: "48% OFF",
+        discounted_price: "487.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12477630sg_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DOLCE & GABBANA",
+        category: "Tank tops",
+        price: "174.00",
+        save: "44%",
+        discounted_price: "96.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/35/35446348CP_14_f.jpg?impolicy=crop&width=387&height=490",
+        title: "DOLCE & GABBANA",
+        category: "Midi skirts",
+        price: "1093.00",
+        save: "52%",
+        discounted_price: "524.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/35/35433200MB_14_f.jpg?impolicy=crop&width=387&height=490",
+        title: "DOLCE & GABBANA",
+        category: "Midi skirts",
+        price: "836.00",
+        save: "40%",
+        discounted_price: "500.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13618661NI_14_f.jpg?impolicy=crop&width=387&height=490",
+        title: "DOLCE & GABBANA",
+        category: "Shorts & Bermuda",
+        price: "796.00",
+        save: "47%",
+        discounted_price: "420.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/42/42808628EV_14_f.jpg?impolicy=crop&width=387&height=490",
+        title: "DOLCE & GABBANA",
+        category: "Denim capris",
+        price: "528.00",
+        save: "33% OFF",
+        discounted_price: "349.00",
+      
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17023749kk_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DOLCE & GABBANA",
+        category: "Ballet flats",
+        price: "666.00",
+        save: "40% OFF",
+        discounted_price: "394.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/38/38968982ev_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DOLCE & GABBANA",
+        category: "Blouses",
+        price: "923.00",
+        save: "41% OFF",
+        discounted_price: "540.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12336834ip_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DOLCE & GABBANA",
+        category: "Hooded sweatshirts",
+        price: "643.00",
+        save: "33% OFF",
+        discounted_price: "429.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13569286bd_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DOLCE & GABBANA",
+        category: "Casual pants",
+        price: "905.00",
+        save: "41% OFF",
+        discounted_price: "533.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/38/38916760ni_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DOLCE & GABBANA",
+        category: "Solid color shirts & blouse",
+        price: "975.00",
+        save: "58% OFF",
+        discounted_price: "405.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12586255CO_14_f.jpg?impolicy=crop&width=387&height=490",
+        title: "DOLCE & GABBANA",
+        category: "T-shirts",
+        price: "804.00",
+        save: "62% OFF",
+        discounted_price: "300.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13028237so_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DOLCE & GABBANA",
+        category: "Casual pants",
+        price: "636.00",
+        save: "57% OFF",
+        discounted_price: "269.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/35/35403409pc_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DOLCE & GABBANA",
+        category: "Mini skirts",
+        price: "803.00",
+        save: "63% OFF",
+        discounted_price: "326.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12692486bj_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DOLCE & GABBANA",
+        category: "Bustiers",
+        price: "891.00",
+        save: "42% OFF",
+        discounted_price: "512.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/48/48281519ii_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "8 BY YOOX",
+        category: "SILK SATIN LONG SLIP NIGHT GOWIN",
+        price: "367.00",
+        save: "38% OFF",
+        discounted_price: "227.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13852235ct_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "8 BY YOOX",
+        category: "Leggins",
+        price: "199.00",
+        save: "0% OFF",
+        discounted_price: "199.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/14/14260169kl_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "RIB KNIT OPEN BACK POLO-NECK BODY SUIT",
+        category: "Sweaters",
+        price: "104.00",
+        save: "0% OFF",
+        discounted_price: "104.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12876811ps_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "PHILOSOPHY DI LORENZO SERAFINI",
+        category: "Sweatshirts",
+        price: "401.00",
+        save: "33% OFF",
+        discounted_price: "268.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13844090bc_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "ALESSANDRA RICH",
+        category: "Denim skirt",
+        price: "747.00",
+        save: "34% OFF",
+        discounted_price: "493.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/45/45687619dm_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "WOVEN BUCKET BAG",
+        category: "Handbags",
+        price: "50.00",
+        save: "0% OFF",
+        discounted_price: "50.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12877075ke_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "MSGM",
+        category: "Sweatshirts",
+        price: "324.00",
+        save: "38% OFF",
+        discounted_price: "200.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13843141ir_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "GANNI",
+        category: "Denim skirts",
+        price: "219.00",
+        save: "34% OFF",
+        discounted_price: "144.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12881329ei_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "KNIT DROP CUT-OUT TOP",
+        category: "Sweaters",
+        price: "80.00",
+        save: "0% OFF",
+        discounted_price: "80.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12875226ik_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "OFF-WHITE",
+        category: "Hooded sweatshirts",
+        price: "847.00",
+        save: "27% OFF",
+        discounted_price: "618.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/45/45687622gj_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "NYLON QUILTED CROSS-BODY BAG",
+        category: "Cross-body bags",
+        price: "40.00",
+        save: "0% OFF",
+        discounted_price: "40.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13838016ah_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "Shorts & Bermuda",
+        category: "Shorts",
+        price: "90.00",
+        save: "0% OFF",
+        discounted_price: "90.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13852233ok_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "RECYCLED NYLON HIGH-WAIST LEGGINGS",
+        category: "Leggings",
+        price: "90.00",
+        save: "0% OFF",
+        discounted_price: "90.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12875764gk_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "STELLA MCCARTNEY",
+        category: "T-shirts",
+        price: "302.00",
+        save: "45% OFF",
+        discounted_price: "210.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/16/16152779mj_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "LOGO MIXED-PATCHES SATIN BOMBER JACKET",
+        category: "Bombers",
+        price: "598.00",
+        save: "0% OFF",
+        discounted_price: "598.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12893394dk_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "MAJE",
+        category: "Tops",
+        price: "100.00",
+        save: "38% OFF",
+        discounted_price: "111.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12874897ih_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "PRINTED JERSEY ROLLNECK TOP",
+        category: "T-shirts",
+        price: "80.00",
+        save: "0% OFF",
+        discounted_price: "80.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17320672oi_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "NEW BALANCE 327",
+        category: "Sneakers",
+        price: "142.00",
+        save: "0% OFF",
+        discounted_price: "142.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17320536xr_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "SEE BY CHLOE",
+        category: "Loafers",
+        price: "466.00",
+        save: "0% OFF",
+        discounted_price: "466.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13821560qk_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "SEE BY CHLOE",
+        category: "Casual pants",
+        price: "378.00",
+        save: "0% OFF",
+        discounted_price: "378.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12854010mn_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "SEE BY CHLOE",
+        category: "T-shirts",
+        price: "212.00",
+        save: "0% OFF",
+        discounted_price: "212.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17320470oq_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "SEE BY CHLOE",
+        category: "Sneakers",
+        price: "230.00",
+        save: "0% OFF",
+        discounted_price: "230.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12849412xi_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "COTTON CROCHET TOP",
+        category: "Tops",
+        price: "70.00",
+        save: "0% OFF",
+        discounted_price: "70.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17325068va_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "VALENTINO GARAVANI",
+        category: "Espadrilles",
+        price: "833.00",
+        save: "37% OFF",
+        discounted_price: "518.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/14/14250025bn_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "8 BY YOOX",
+        category: "Sweaters",
+        price: "80.00",
+        save: "0% OFF",
+        discounted_price: "80.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12870104us_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "8 BY YOOX",
+        category: "Silk shirts & blouses",
+        price: "130.00",
+        save: "0% OFF",
+        discounted_price: "130.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/16/16146593ka_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "8 BY YOOX",
+        category: "Blazers",
+        price: "160.00",
+        save: "0% OFF",
+        discounted_price: "160.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17308135ku_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "LEATHER SQUARE TOE PENNY LOAFER",
+        category: "Loafers",
+        price: "160.00",
+        save: "0% OFF",
+        discounted_price: "160.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17331048ub_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "LEATHER CHUNKY HEELED LOAFER",
+        category: "Loafers",
+        price: "200.00",
+        save: "0% OFF",
+        discounted_price: "200.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/15/15226350fs_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "PLEATED SHIRT MINI DRESS",
+        category: "Short dresses",
+        price: "90.00",
+        save: "0% OFF",
+        discounted_price: "90.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/14/14255473ip_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "8 BY YOOX",
+        category: "Sweaters",
+        price: "100.00",
+        save: "0% OFF",
+        discounted_price: "100.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17307054fn_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "ANKLE BOOTS",
+        category: "Boots",
+        price: "220.00",
+        save: "0% OFF",
+        discounted_price: "220.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/14/14255440lt_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "* BY YOOX",
+        category: "Cardigangs",
+        price: "130.00",
+        save: "0% OFF",
+        discounted_price: "130.00",
+    },
+];
+
+
+
+let orderDataArr = JSON.parse(localStorage.getItem("selectWomenProduct")) || [];
+let container = document.getElementById("container");
+
+let appendData = (data) => {
+    container.innerHTML = null;
+    data.forEach((elem) => {
+        let div = document.createElement("div");
+        div.id = "box";
+        let img = document.createElement("img");
+        img.src = elem.image;
+        let title = document.createElement("h4");
+        title.innerText = elem.title;
+        let categories = document.createElement("p");
+        categories.innerText = elem.category;
+        let price = document.createElement("p");
+        price.innerText = +elem.price;
+        let save = document.createElement("p");
+        save.innerText = elem.save;
+        let discount = document.createElement("p");
+        discount.innerText = +elem.discounted_price;
+        let btn = document.createElement("button");
+        btn.innerText = "Add to Cart";
+        btn.addEventListener("click", function () {
+            addDetails(elem);
+        });
+
+        div.append(img, title, categories, price, save, discount, btn);
+        // console.log(div);
+
+        container.append(div);
+    });
+};
+
+appendData(women);
+
+
+function addDetails(elem) {
+    let data1 = {
+        img: elem.image,
+        title: elem.title,
+        categories: elem.category,
+        price: elem.price,
+        save: elem.save,
+        discount: elem.discounted_price,
+    };
+    
+    orderDataArr.push(data1);
+    // console.log(data1);
+    localStorage.setItem("selectWomenProduct", JSON.stringify(orderDataArr));
+    alert("Item added to the Cart....");
+}
+
+
+//price select options
+
+let change = () => {
+    // console.log(document.getElementById("sortBy").value);
+    let changeValue = document.getElementById("sortBy").value;
+    if (changeValue == "Lowest_Price") {
+        sortLH();
+    } else if (changeValue == "Highest_Price") {
+        sortHL();
+    }
+}
+
+let sortLH = () => {
+    // console.log("Low to High");
+    women = women.sort(function (a, b) {
+        return a.discounted_price - b.discounted_price;
+    });
+    // console.log(men)
+    appendData(women);
+}
+
+let sortHL = () => {
+    // console.log("High to Low");
+    women = women.sort(function (a, b) {
+        return b.discounted_price - a.discounted_price;
+    });
+    // console.log(men)
+    appendData(women);
+}
+
+
+
+
+
+
+
