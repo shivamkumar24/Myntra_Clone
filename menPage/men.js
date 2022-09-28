4359e73a43d4f55c6a0fdf085e19fbb28b8d4146
Shivam Kumar <shivam2432000@gmail.com>
2022-09-29 00:12:49 +0530

    This is my Day-2 work

---
+++ menPage/men.js
@@ -0,0 +1,34 @@
+
+document.querySelector("#banner").addEventListener("click",()=>{
+    window.location.href="menProduct.html";
+})
+
+document.querySelector("#omg").addEventListener("click",()=>{
+    window.location.href="menProduct.html";
+})
+
+document.querySelector("#badget").addEventListener("click",()=>{
+    window.location.href="menProduct.html";
+})
+
+document.querySelector("#top_categories").addEventListener("click",()=>{
+    window.location.href="menProduct.html";
+})
+
+document.querySelector("#top_brand").addEventListener("click",()=>{
+    window.location.href="menProduct.html";
+})
+
+document.querySelector("#shop_categories").addEventListener("click",()=>{
+    window.location.href="menProduct.html";
+})
+
+
+
+
+
+
+
+
+
+
---
+++ menPage/menProduct.js
@@ -0,0 +1,473 @@
+
+let men = [
+    {
+        image: "https://www.yoox.com/images/items/12/12872026gp_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "BRUNELLO CUCINELLI",
+        category: "Solid color shirts",
+        price: "666.00",
+        save: "30% OFF",
+        discounted_price: "466.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/47/47312605tl_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "BIKKEMBERGS",
+        category: "Swim shorts",
+        price: "133.00",
+        save: "39% OFF",
+        discounted_price: "81.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12418674we_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "VERSACE COLLECTION",
+        category: "Polo shirts",
+        price: "233.00",
+        save: "46% OFF",
+        discounted_price: "125.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12871690rh_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "ADIDAS ORIGINALS",
+        category: "T-shirts",
+        price: "32.00",
+        save: "25% OFF",
+        discounted_price: "24.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12878680rc_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "OBEY",
+        category: "T-shirts",
+        price: "82.00",
+        save: "19% OFF",
+        discounted_price: "66.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13847491dl_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "AMI ALEXANDRE MATTIUSSI",
+        category: "Shorts & Bermuda",
+        price: "227.00",
+        save: "34% OFF",
+        discounted_price: "149.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12469590te_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "MARKET",
+        category: "T-shirts",
+        price: "66.00",
+        save: "33% OFF",
+        discounted_price: "44.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12742915wi_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "TOMMY HILFIGER",
+        category: "T-shirts",
+        price: "98.00",
+        save: "54% OFF",
+        discounted_price: "45.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12867207cx_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DIESEL",
+        category: "T-shirts",
+        price: "95.00",
+        save: "29% OFF",
+        discounted_price: "67.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13837238lc_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DIESEL",
+        category: "Shorts & Bermunda",
+        price: "160.00",
+        save: "40% OFF",
+        discounted_price: "96.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13843431kq_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "EA7",
+        category: "Casual pants",
+        price: "204.00",
+        save: "47% OFF",
+        discounted_price: "108.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12867407me_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DIESEL",
+        category: "Tanktops",
+        price: "55.00",
+        save: "10% OFF",
+        discounted_price: "49.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13835103jt_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "RICK OWENS",
+        category: "Denim capris",
+        price: "605.00",
+        save: "46% OFF",
+        discounted_price: "325.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13849369ww_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "VERSACE JEANS COUTURE",
+        category: "5-pocket",
+        price: "339.00",
+        save: "40% OFF",
+        discounted_price: "201.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13846495sc_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "MANUEL RITZ",
+        category: "Shorts & Bermuda",
+        price: "130.00",
+        save: "50% OFF",
+        discounted_price: "65.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/47/47290193bn_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "LANVIN",
+        category: "Swim shorts",
+        price: "601.00",
+        save: "42% OFF",
+        discounted_price: "347.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12880070wi_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "EMPORIO ARMANI",
+        category: "Checked shirts",
+        price: "224.00",
+        save: "33% OFF",
+        discounted_price: "148.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12418674we_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "VERSACE COLLECTION",
+        category: "Polo shirts",
+        price: "233.00",
+        save: "46% OFF",
+        discounted_price: "125.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/14/14259208kt_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "Z ZEGNA",
+        category: "Sweaters",
+        price: "233.00",
+        save: "49% OFF",
+        discounted_price: "118.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12878967bf_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "PAL ZILERI",
+        category: "Patterned shirts",
+        price: "260.00",
+        save: "36% OFF",
+        discounted_price: "165.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13617769gk_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "VELENTINO",
+        category: "Denim pants",
+        price: "773.00",
+        save: "54% OFF",
+        discounted_price: "348.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13750225sq_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "VERSACE JEANS COUTURE",
+        category: "5-pocket",
+        price: "537.00",
+        save: "40% OFF",
+        discounted_price: "322.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12809551in_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "MSGM",
+        category: "Hooded sweatshirts",
+        price: "241.00",
+        save: "45% OFF",
+        discounted_price: "131.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13847491dl_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "AMI ALEXANDRE MATTIUSSI",
+        category: "Shorts & Bermuda",
+        price: "227.00",
+        save: "34% OFF",
+        discounted_price: "149.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13768248uo_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "VALENTINO",
+        category: "Casual pants",
+        price: "994.00",
+        save: "60% OFF",
+        discounted_price: "396.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12875233eo_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "BALENCIAGA",
+        category: "T-shirts",
+        price: "726.00",
+        save: "35% OFF",
+        discounted_price: "466.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12760460uu_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "BALMAIN",
+        category: "T-shirts",
+        price: "540.00",
+        save: "27% OFF",
+        discounted_price: "390.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12418674we_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "VERSACE COLLECTION",
+        category: "Polo shirts",
+        price: "233.00",
+        save: "46% OFF",
+        discounted_price: "125.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12325542rq_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DOLCE & GABBANA",
+        category: "T-shirts",
+        price: "350.00",
+        save: "26% OFF",
+        discounted_price: "258.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/38/38769283jm_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "VALENTINO",
+        category: "Patterned shirts",
+        price: "959.00",
+        save: "44% OFF",
+        discounted_price: "530.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12481687dm_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "GUCCI",
+        category: "T-shirts",
+        price: "645.00",
+        save: "14% OFF",
+        discounted_price: "549.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12890771bw_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "CALVIN KLEIN",
+        category: "Sweatshirts",
+        price: "136.00",
+        save: "46% OFF",
+        discounted_price: "73.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17334346dn_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "VALENTINO GARAVANI",
+        category: "Sneakers",
+        price: "954.00",
+        save: "40% OFF",
+        discounted_price: "572.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12325542rq_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "DOLCE & GABBANA",
+        category: "T-shits",
+        price: "350.00",
+        save: "26% OFF",
+        discounted_price: "258.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17319272cm_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "GIUSEPPE ZANOTTI",
+        category: "Loafers",
+        price: "806.00",
+        save: "42% OFF",
+        discounted_price: "465.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17319261dr_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "GIUSEPPE ZANOTTI",
+        category: "Loafers",
+        price: "806.00",
+        save: "42% OFF",
+        discounted_price: "465.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12777393jl_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "SAINT LAURENT",
+        category: "Polo shirts",
+        price: "611.00",
+        save: "36% OFF",
+        discounted_price: "391.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/11/11300529vu_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "LANVIN",
+        category: "Sneakers",
+        price: "367.00",
+        save: "20% OFF",
+        discounted_price: "284.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12762190ot_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "ALEXANDER MCQUEEN",
+        category: "T-shirts",
+        price: "402.00",
+        save: "36% OFF",
+        discounted_price: "304.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17325033fa_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "LANVIN",
+        category: "Sneakers",
+        price: "619.00",
+        save: "34% OFF",
+        discounted_price: "405.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17336458pf_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "LANVIN",
+        category: "Sneakers",
+        price: "600.00",
+        save: "34% OFF",
+        discounted_price: "389.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/17/17325155lf_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "LANVIN",
+        category: "Sneakers",
+        price: "664.00",
+        save: "34% OFF",
+        discounted_price: "435.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12881874jw_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "VALENTINO",
+        category: "T-shirts",
+        price: "906.00",
+        save: "30% OFF",
+        discounted_price: "628.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/46/46753858wf_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "Y-3",
+        category: "Hats",
+        price: "98.00",
+        save: "23% OFF",
+        discounted_price: "75.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/12/12878581vg_14_f.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "PAL ZILERI",
+        category: "Patterned shirts",
+        price: "178.00",
+        save: "35% OFF",
+        discounted_price: "114.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/13/13843615fr_14_r.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "Z ZEGNA",
+        category: "Shorts & Bermuda",
+        price: "252.00",
+        save: "50% OFF",
+        discounted_price: "124.00",
+    },
+    {
+        image: "https://www.yoox.com/images/items/16/16149858rx_14_e.jpg?width=210&height=272&impolicy=crop&gravity=Center",
+        title: "Z ZEGNA",
+        category: "Jackets",
+        price: "530.00",
+        save: "56% OFF",
+        discounted_price: "232.00",
+    },
+
+]
+
+
+
+// let orderDataArr=JSON.parse(localStorage.getItem("selectMenProduct")) || [];
+
+let container = document.getElementById("container");
+
+let appendData = (data) => {
+    container.innerHTML = null;
+    data.forEach((elem) => {
+        let div = document.createElement("div");
+        div.id = "box";
+        let img = document.createElement("img");
+        img.src = elem.image;
+        let title = document.createElement("h4");
+        title.innerText = elem.title;
+        let categories = document.createElement("p");
+        categories.innerText = elem.category;
+        let price = document.createElement("p");
+        price.innerText = +elem.price;
+        let save = document.createElement("p");
+        save.innerText = elem.save;
+        let discount = document.createElement("p");
+        discount.innerText = +elem.discounted_price;
+        let btn=document.createElement("button");
+        btn.innerText="Add to Cart";
+
+        div.append(img, title, categories, price, save, discount,btn);
+        // console.log(div);
+
+        // select product
+        btn.onclick = () => {
+            let data1 = {
+                img: elem.image,
+                title: elem.title,
+                categories: elem.category,
+                price: elem.price,
+                save: elem.save,
+                discount: elem.discounted_price,
+            };
+
+            // orderDataArr.push(data1);
+            // console.log(data1);
+            localStorage.setItem("selectMenProduct", JSON.stringify(data1));
+            // window.location = "./menProduct.html";
+        }
+
+        container.append(div);
+    });
+};
+
+appendData(men);
+
+
+
+
+
+//price select options
+
+let change = () => {
+    // console.log(document.getElementById("sortBy").value);
+    let changeValue = document.getElementById("sortBy").value;
+    if (changeValue == "Lowest_Price") {
+        sortLH();
+    } else if (changeValue == "Highest_Price") {
+        sortHL();
+    }
+}
+
+let sortLH = () => {
+    // console.log("Low to High");
+    men = men.sort(function (a, b) {
+        return a.discounted_price - b.discounted_price;
+    });
+    // console.log(men)
+    appendData(men);
+}
+
+let sortHL = () => {
+    // console.log("High to Low");
+    men = men.sort(function (a, b) {
+        return b.discounted_price - a.discounted_price;
+    });
+    // console.log(men)
+    appendData(men);
+}
+
+
+
+
+
+
+
