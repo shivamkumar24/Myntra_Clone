62fe46ef8201a89d1a85e08ea00e8347caf8c0e6
Shivam Kumar <shivam2432000@gmail.com>
2022-09-29 17:26:57 +0530

    This is My Day-5 Work

---
+++ cartPage/address.js
@@ -0,0 +1,46 @@
+
+let value=JSON.parse(localStorage.getItem("emailData")) || ["Shivam"];
+
+document.getElementById("Reg_email").innerText="Hello, "+value;
+
+function userAddress(n,p,e,a,p,c,s,c){
+    this.name=n;
+    this.phone=p;
+    this.email=e;
+    this.address=a;
+    this.pin=p;
+    this.city=c;
+    this.state=s;
+    this.country=c;
+}
+
+
+let orderAddress=JSON.parse(localStorage.getItem("Address")) || [];
+let arr=[];
+function orderData(event){
+    event.preventDefault();
+    let form=document.getElementById("user_datails");
+    let name=form.name.value;
+    let phone=form.phone.value;
+    let email=form.email.value;
+    let address=form.address.value;
+    let pin=form.pincode.value;
+    let city=form.city.value;
+    let state=form.state.value;
+    let country=form.country.value;
+
+    let user_address=new userAddress(name,phone,email,address,pin,city,state,country);
+
+    if(user_address.name=="" || user_address.phone=="" || user_address.email=="" || user_address.address=="" || user_address.pin=="" || user_address.city=="" || user_address.state=="" || user_address.country==""){
+        alert("Please fill all Address details.....");
+    }else{
+        arr.push(user_address);
+        console.log(arr);
+        localStorage.setItem("Address",JSON.stringify(arr));
+
+        alert("Confirm Address ....");
+        window.location.href="checkout.html";
+    }
+}
+
+
---
+++ cartPage/cart.js
@@ -0,0 +1,50 @@
+
+
+let cartArr = JSON.parse(localStorage.getItem("selectProduct")) || [];
+let total = 0;
+let Qty = 0;
+
+//BrushesArr Access
+cartArr.forEach(function (elem, index) {
+    let box = document.createElement("div");
+    let image = document.createElement("img");
+    let name = document.createElement("h3");
+    let price = document.createElement("h2");
+    let del = document.createElement("button");
+    image.setAttribute("src", elem.img);
+    name.innerText = elem.title;
+    price.innerText = elem.price;
+    del.innerText = "Delete";
+    del.addEventListener("click", function () {
+        deleteContent(elem, index);
+    });
+    total = total + Number(elem.price);
+    Qty++;
+    box.append(image, name, price, del);
+    document.querySelector("#container").append(box);
+});
+// console.log(total);
+// console.log(Qty);
+
+function deleteContent(elem, index) {
+    total = total - Number(elem.price);
+    Qty--;
+    cartArr.splice(index, 1);
+    localStorage.setItem("selectProduct", JSON.stringify(cartArr));
+    
+}
+
+document.querySelector("#totalAmount").innerText = total;
+document.querySelector("#totalQuantity").innerText = Qty;
+
+
+
+
+document.getElementById("shopping_continue").addEventListener("click",function(){
+    window.location.href="address.html";
+});
+
+
+
+
+
